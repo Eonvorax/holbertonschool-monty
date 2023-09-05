5fdeb54be13e1c6fa789e5d3b71ee9e0ce4a3927
Eonvorax <130477483+Eonvorax@users.noreply.github.com>
2023-09-05 10:23:06 +0000

    Extended main and created basic functions

--- main.c
+++ main.c
@@ -1,5 +1,70 @@
 #include "monty.h"
 
+char *buffer = NULL;
+
+/**
+ * free_stack - frees each element of the stack
+ * @top: pointer to top element of the stack
+ *
+ * Return: nothing
+*/
+void free_stack(stack_t **top)
+{
+    stack_t *node;
+
+    if (top == NULL)
+        return;
+    node = *top;
+    while (node != NULL)
+    {
+        *top = (*top)->prev;
+        free(node);
+        node = *top;
+    }
+}
+
+/**
+ * pall - prints the stack from top to bottom
+ * @top: pointer to top element of the stack
+ * @line_number: line number of the current opcode
+ *
+ * Return: nothing
+ */
+void pall(stack_t **top, unsigned int line_number)
+{
+    stack_t *node;
+
+    if (top == NULL)
+        return;
+    node = *top;
+    while (node != NULL)
+    {
+        printf("%d\n", node->n);
+        node = node->prev;
+    }
+}
+
+/**
+ * push - adds an element to the stack
+ * @top: pointer to top element of the stack
+ * @line_number: line number of the current opcode
+ * Return: address of added node
+ */
+void push(stack_t **top, unsigned int line_number)
+{
+    stack_t *node = NULL;
+    char *line = NULL;
+
+    if (top == NULL)
+    {
+        fprintf(stderr, "L%d: usage: push integer", line_number);
+        exit(EXIT_FAILURE);
+    }
+    fprintf(stderr, "L%d: usage: push integer", line_number);
+
+    node->n = atoi(strtok(NULL, " "));
+}
+
 /**
  * main - for testing purposes
  * @argc: number of arguments passed to main
@@ -10,6 +75,15 @@
 int main(int argc, char *argv[])
 {
     FILE *fp;
+    size_t max_line_len = 4096;
+    unsigned int i = 0;
+    char *token = NULL;
+    stack_t **top = NULL;
+    instruction_t opcodes[] = {
+        {"push", push},
+        {"pall", pall},
+        {NULL, NULL}};
+
     if (argc != 2)
     {
         perror("USAGE: monty file\n");
@@ -21,5 +95,21 @@ int main(int argc, char *argv[])
         fprintf(stderr, "Error: Can't open file %s", argv[1]);
         exit(EXIT_FAILURE);
     }
+    while (getline(&buffer, &max_line_len, fp))
+    {
+        buffer[strlen(buffer) - 1] = '\0';
+        /* find a match in opcodes[]*/
+        while (opcodes[i].opcode != NULL)
+        {
+            token = strtok(buffer, " ");
+            if (strcmp(token, opcodes[i].opcode) == 0)
+            {
+                opcodes[i].f(top, i + 1);
+            }
+            i++;
+        }
+    }
+    fclose(fp);
+    /* free linked list */
     return (0);
 }
