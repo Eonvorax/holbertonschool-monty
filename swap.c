23cb648fc806a5c5bf7cebec01ce7f90d5a927d8
Cathy Augustin <135758000+HolbyKate@users.noreply.github.com>
2023-09-07 12:16:53 +0000

    Fix : checking before swap

--- swap.c
+++ swap.c
@@ -10,15 +10,14 @@
 
 void swap(stack_t **top, unsigned int line_number)
 {
-    int swp;
-    swp = (*top)->n;
-    (*top)->n = (*top)->prev->n;
-    (*top)->prev->n = swp;
-
     if (*top == NULL || (*top)->prev == NULL)
     {
         printf("L%d: can't swap, stack too short\n", line_number);
         free(buffer);
         exit(EXIT_FAILURE);
     }
+    int swp;
+    swp = (*top)->n;
+    (*top)->n = (*top)->prev->n;
+    (*top)->prev->n = swp;
 }
